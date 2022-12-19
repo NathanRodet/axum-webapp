9cbaa9de53337d301d95e4b07ad1542581214a8b
Nathan Rodet <nrodet@grandlyon.com>
2022-12-19 08:58:03 +0100

    Corrected routes path

--- src/router.rs
+++ src/router.rs
@@ -12,23 +12,23 @@ pub async fn create_routes(app_state: AppState) -> Router {
     //     .route("/:id", get(get_task).put(update_task).delete(delete_task));
     // https://docs.rs/axum/0.2.3/axum/routing/struct.Router.html
 
-    let unlogged_nest = Router::new()
+    let guest_nest = Router::new()
         .route("/", get(hello_world))
         .route("/login", post(login))
         .route("/register", post(create_user))
         .route("/renew_auth", post(renew_auth));
 
-    let logged_nest = Router::new()
-        .route("", post(create_task).get(get_all_tasks))
+    let user_nest = Router::new()
+        .route("/", post(create_task).get(get_all_tasks))
         .route("/:id", get(get_task).put(update_task).delete(delete_task));
 
     let admin_nest = Router::new()
-        .route("", get(get_all_users))
+        .route("/", get(get_all_users))
         .route("/:username", delete(delete_user_by_username));
 
     Router::new()
-        .nest("/", unlogged_nest)
-        .nest("/task", logged_nest)
+        .nest("", guest_nest)
+        .nest("/task", user_nest)
         .nest("/user", admin_nest)
         .with_state(app_state)
 }
